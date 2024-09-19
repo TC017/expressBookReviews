19405723e1164bd3cd1bf813b659e23a61443eed
tc017 <thomas.cherian@gmail.com>
2024-09-19 06:05:01 +0000

    Task 7 complete

--- final_project/index.js
+++ final_project/index.js
@@ -22,6 +22,20 @@ const doesExist = (username) => {
   }
 };
 
+// Check if the user with the given username and password exists
+const authenticatedUser = (username, password) => {
+  // Filter the users array for any user with the same username and password
+  let validusers = users.filter((user) => {
+    return user.username === username && user.password === password;
+  });
+  // Return true if any valid user is found, otherwise false
+  if (validusers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 app.use(express.json());
 
 app.use(
@@ -49,6 +63,40 @@ app.use("/customer/auth/*", function auth(req, res, next) {
     });
   } else {
     return res.status(403).json({ message: "User not logged in" });
+  }
+});
+
+// Login endpoint
+app.post("/customer/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if username or password is missing
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+
+  // Authenticate user
+  if (authenticatedUser(username, password)) {
+    // Generate JWT access token
+    let accessToken = jwt.sign(
+      {
+        data: password,
+      },
+      "access",
+      { expiresIn: 60 }
+    );
+
+    // Store access token and username in session
+    req.session.authorization = {
+      accessToken,
+      username,
+    };
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res
+      .status(208)
+      .json({ message: "Invalid Login. Check username and password" });
   }
 });
 
