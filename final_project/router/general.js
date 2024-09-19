609eb4b5b84cce6c5301ecbd440183fbf4168784
tc017 <thomas.cherian@gmail.com>
2024-09-19 05:55:10 +0000

    general task 6 complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,14 +35,21 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+  const filteredBooks = Object.keys(books).reduce((acc, isbn) => {
+    if (books[isbn].title.toLowerCase() === title.toLowerCase()) {
+      acc[isbn] = books[isbn];
+    }
+    return acc;
+  }, {});
+  res.set("Content-Type", "application/json");
+  res.send(JSON.stringify(filteredBooks, null, 4));
 });
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  res.send(JSON.stringify(books[req.params.isbn].reviews, null, 4));
 });
 
 module.exports.general = public_users;
